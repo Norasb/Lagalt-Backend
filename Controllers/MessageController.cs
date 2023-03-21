525685796baf23d0a36be5ee8dbbb7e1034e68f3
Norasb <norasophie96@hotmail.com>
2023-03-21 13:27:22 +0100

    Feat: Created Dto's for Message

--- Controllers/MessageController.cs
+++ Controllers/MessageController.cs
@@ -1,4 +1,6 @@
-﻿using Lagalt_Backend.Models.Domain;
+﻿using AutoMapper;
+using Lagalt_Backend.Models.Domain;
+using Lagalt_Backend.Models.Dto.Message;
 using Lagalt_Backend.Services.Messages;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
@@ -10,25 +12,30 @@ namespace Lagalt_Backend.Controllers
     public class MessageController : ControllerBase
     {
         private readonly IMessageService _messageService;
+        private readonly IMapper _mapper;
 
-        public MessageController(IMessageService messageService)
+        public MessageController(IMessageService messageService, IMapper mapper)
         {
             _messageService = messageService;
+            _mapper = mapper;
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Message>>> GetAllMessages()
+        public async Task<ActionResult<IEnumerable<MessageDto>>> GetAllMessages()
         {
             return Ok(
-                await _messageService.GetAllAsync());
+                _mapper.Map<List<MessageDto>>(
+                await _messageService.GetAllAsync()));
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Message>> GetMessageById(int id)
+        public async Task<ActionResult<MessageDto>> GetMessageById(int id)
         {
             try
             {
-                return Ok(await _messageService.GetByIdAsync(id));
+                return Ok(
+                    _mapper.Map<MessageDto>(
+                    await _messageService.GetByIdAsync(id)));
 
             } catch (Exception ex)
             {
@@ -42,18 +49,19 @@ namespace Lagalt_Backend.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult> AddMessage(Message message)
+        public async Task<ActionResult> AddMessage(MessagePostDto postMessage)
         {
+            Message message = _mapper.Map<Message>(postMessage);
             await _messageService.AddAsync(message);
             return CreatedAtAction("GetMessageById", new { id = message.Id }, message);
         }
 
         [HttpPut("{id}")]
-        public async Task<ActionResult> UpdateMessage(int id, Message message)
+        public async Task<ActionResult> UpdateMessage(int id, MessagePutDto putMessage)
         {
             try
             {
-                await _messageService.UpdateAsync(message);
+                await _messageService.UpdateAsync(_mapper.Map<Message>(putMessage));
                 return NoContent();
             } catch (Exception ex)
             {
---
+++ Models/Dto/Message/MessagePutDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Lagalt_Backend.Models.Dto.Message
+{
+    public class MessagePutDto
+    {
+        public int Id { get; set; }
+        public string Text { get; set; } = null!;
+    }
+}
