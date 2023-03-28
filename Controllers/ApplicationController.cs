5f822e6f4fc962fa355482348e69cc5e02729693
Tine-Lovise Storvoll <tinelovisestorvoll@gmail.com>
2023-03-28 10:43:50 +0200

    feat: implement GetNotApprovedApplications() method for getting applications that has not been approved for a given project

--- Controllers/ApplicationController.cs
+++ Controllers/ApplicationController.cs
@@ -107,5 +107,12 @@ namespace Lagalt_Backend.Controllers
 
             
         }
+
+        // GET: api/Application/
+        [HttpGet("notapproved")]
+        public async Task<ActionResult<ApplicationDTO>> GetNotApprovedApplications(int projectId)
+        {
+            return Ok(_mapper.Map<List<ApplicationStatusDTO>>(await _applicationService.GetNotApprovedApplications(projectId)));
+        }
     }
 }
